fa0ab818546f9fbc9c57d1190ec5ebf321dd7727
vokur13 <vokur13@gmail.com>
2022-08-09 18:55:40 +0300

    task structure created

--- js/task-07.js
+++
@@ -1,8 +0,0 @@
-const controlInput = document.querySelector('#font-size-control');
-const textOutput = document.querySelector('#text');
-
-controlInput.addEventListener('input', changeFontSize);
-
-function changeFontSize(event) {
-  textOutput.style.fontSize = `${event.currentTarget.value}px`;
-}
