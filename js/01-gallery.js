a81d657566af741d99828fe15912ad34c0b701fc
vokur13 <vokur13@gmail.com>
2022-08-10 19:14:49 +0300

    Создание и рендер разметки по массиву данных galleryItems и предоставленному шаблону элемента галереи

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,25 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const container = document.querySelector('.gallery');
+const pictureMap = createPictureGalleryMarkup(galleryItems);
+// console.log(galleryItems);
+
+container.insertAdjacentHTML('beforeend', pictureMap);
+
+function createPictureGalleryMarkup(gallery) {
+  return gallery
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+        <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+        />
+        </a>
+        </div>`;
+    })
+    .join('');
+}
