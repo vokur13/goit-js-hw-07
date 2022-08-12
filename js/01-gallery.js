816b8c69dac9c9fc44ac1fe0d8b3963820583784
vokur13 <vokur13@gmail.com>
2022-08-12 19:52:54 +0300

    npm install basiclightbox

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -5,6 +5,8 @@ const container = document.querySelector('.gallery');
 const pictureMap = createPictureGalleryMarkup(galleryItems);
 // console.log(galleryItems);
 
+container.addEventListener('click', onContainerClick);
+
 container.insertAdjacentHTML('beforeend', pictureMap);
 
 function createPictureGalleryMarkup(gallery) {
@@ -23,3 +25,26 @@ function createPictureGalleryMarkup(gallery) {
     })
     .join('');
 }
+
+function onContainerClick(e) {
+  e.preventDefault();
+  const isGalleryItemEl = e.target.classList.contains('gallery__image');
+  if (!isGalleryItemEl) {
+    return;
+  }
+  const linkEl = e.target;
+  const originalURL = linkEl.dataset.source;
+  console.log('originalURL:', originalURL);
+}
+
+//
+
+document.querySelector('button.image').onclick = () => {
+  basicLightbox
+    .create(
+      `
+		<img width="1400" height="900" src="https://placehold.it/1400x900">
+	`
+    )
+    .show();
+};
